afb83e41f0159d36398321e68cc22e09e0c51f71
Brian Ko <brian.kostar@gmail.com>
2018-10-20 23:05:46 -0400

    update contract

--- pet-shop/contracts/Adoption.sol
+++ pet-shop/contracts/Adoption.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.4.17;
 
 contract Adoption {
-    /// @dev array of eth  addresses
+    /// array of eth  addresses
     address[16] public adopters;
 
-    /// @dev: set pet's adopter to be their address if petId is valid
+    /// set pet's adopter to be their address if petId is valid
     function adopt(uint petId) public returns (uint) {
         require((petId >= 0 && petId < 15), "Invalid Pet Id");
         adopters[petId] = msg.sender;
         return petId;
     }
 
-    /// @dev: return all of adopters array
+    /// return all of adopters array
     function getAdopters() public view returns (address[16]){
         return adopters;
     }
